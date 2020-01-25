bbe976a88b5487af44c25c8f076ad09f84864a32
igor <ilaborie@gmail.com>
2020-01-25 12:57:22 +0100

    Improve image sizing

--- tools/images.js
+++ tools/images.js
@@ -7,19 +7,22 @@ Logger.config = colorEmojiConfig;
 const logger = Logger.getLogger('images');
 logger.level = LogLevel.All;
 
-const imagesFiles = [
-  {files: `static/images/team/*.*`, width: 150, format: 'webp'},
-  {files: `static/images/partners/**/*.*`, height: 180, format: 'webp'},
-  {files: `static/images/album/**/*.*`, format: 'webp'},
-  {files: `static/images/wtf/*.*`, format: 'webp'},
-  {files: `static/images/backgrounds/**/*.*`, format: 'webp'},
-  {files: `static/images/*.jpg`, format: 'webp'}
-];
-
 const failure = msg => err => {
   logger.error(msg);
   console.error(err);
 };
+
+
+// Resize and convert
+const imagesFiles = [
+  {files: `static/images/album/**/*.*`, format: 'webp'},
+  {files: `static/images/backgrounds/*.*`, format: 'webp'},
+  {files: `static/images/logos/*_text*.*`, width: 640, format: 'png'},
+  {files: `static/images/partners/**/*.*`, height: 180, format: 'webp'},
+  {files: `static/images/team/*.*`, width: 150, format: 'webp'},
+  {files: `static/images/wtf/*.*`, format: 'webp'},
+  {files: `static/images/*.jpg`, format: 'webp'}
+];
 
 const computeOptions = ({file, width, height, format}) => {
   if (width && height)
@@ -52,3 +55,54 @@ imagesFiles.forEach(({files, width, height, format}) => {
     });
 });
 
+
+// Create alternative image in different resolutions
+const baseWidths = [1140, 960, 720, 540];
+const imagesAltFiles = [
+  `static/images/backgrounds/*.jpg`,
+  `static/images/kids/*.jpg`,
+  `static/images/wtf/*.jpg`,
+  `static/images/social-share.jpg`
+];
+
+imagesAltFiles.forEach(files => {
+  logger.info(`Deal with ${files} and widths: ${baseWidths}`);
+  glob(files)
+    .forEach(file => {
+      const {dir, name} = path.parse(file);
+
+      baseWidths.forEach(width => {
+        const output = path.format({dir, name: `${name}-${width}`, ext: `.webp`});
+
+        sharp(file).resize({width})
+          .toFile(output)
+          .catch(failure(`Fail to generate ${output}`))
+          .then(() => logger.info(`Generate ${output}`, '[OK]'));
+      });
+
+    });
+});
+
+// Albums
+glob(`static/images/album/**/*.jpg`)
+  .forEach(file => {
+    const {dir, name} = path.parse(file);
+    baseWidths.forEach(width => {
+      const output = path.format({dir, name: `${name}-${width}`, ext: `.webp`});
+      sharp(file)
+        .resize({width: width / 5 * 2}) // half
+        .toFile(output)
+        .catch(failure(`Fail to generate ${output}`))
+        .then(() => logger.info(`Generate ${output}`, '[OK]'));
+    });
+  });
+
+// Map
+baseWidths.forEach(width => {
+  const output = path.format({dir: 'static/images/', name: `map-${width}`, ext: `.webp`});
+  sharp(`static/images/map.jpg`)
+    .resize({width, height: 480, fit: 'cover'})
+    .toFile(output)
+    .catch(failure(`Fail to generate ${output}`))
+    .then(() => logger.info(`Generate ${output}`, '[OK]'));
+});
