2c7b3ce59c722e37d9b439fa833e3eb3aa107a57
igor <ilaborie@gmail.com>
2020-02-03 22:20:42 +0100

    Update for scripts

---
+++ src/scripts/main.js
@@ -0,0 +1,3 @@
+import '../../themes/devfest-theme-hugo/src/scripts/main';
+
+import './easter';
--- tools/scripts.js
+++ tools/scripts.js
@@ -1,5 +1,5 @@
 const rollup = require('rollup');
-const resolve = require('rollup-plugin-node-resolve');
+const uglify = require("rollup-plugin-uglify").uglify;
 const babel = require('rollup-plugin-babel');
 const {Logger, LogLevel, colorEmojiConfig} = require('plop-logger');
 
@@ -7,15 +7,9 @@ Logger.config = colorEmojiConfig;
 const logger = Logger.getLogger('scripts');
 logger.level = LogLevel.All;
 
-const globals = {firebase: 'firebase'};
-
-const config = {
+const inputOptions = {
   input: 'src/scripts/main.js',
-  output: [
-    {file: 'static/theme.js', format: 'esm', globals}
-  ],
   plugins: [
-    resolve(),
     babel({
       exclude: 'node_modules/**' // only transpile our source code
     }),
@@ -30,53 +24,16 @@ const config = {
   }
 };
 
+const outputOptions = {
+  file: 'static/main.js',
+  format: 'esm',
+  globals: {firebase: 'firebase'}
+};
+
 
 async function build() {
-  // create a bundle
   const bundle = await rollup.rollup(inputOptions);
-
-  console.log(bundle.watchFiles); // an array of file names this bundle depends on
-
-  // generate code
-  const { output } = await bundle.generate(outputOptions);
-
-  for (const chunkOrAsset of output) {
-    if (chunkOrAsset.type === 'asset') {
-      // For assets, this contains
-      // {
-      //   fileName: string,              // the asset file name
-      //   source: string | Buffer        // the asset source
-      //   type: 'asset'                  // signifies that this is an asset
-      // }
-      console.log('Asset', chunkOrAsset);
-    } else {
-      // For chunks, this contains
-      // {
-      //   code: string,                  // the generated JS code
-      //   dynamicImports: string[],      // external modules imported dynamically by the chunk
-      //   exports: string[],             // exported variable names
-      //   facadeModuleId: string | null, // the id of a module that this chunk corresponds to
-      //   fileName: string,              // the chunk file name
-      //   imports: string[],             // external modules imported statically by the chunk
-      //   isDynamicEntry: boolean,       // is this chunk a dynamic entry point
-      //   isEntry: boolean,              // is this chunk a static entry point
-      //   map: string | null,            // sourcemaps if present
-      //   modules: {                     // information about the modules in this chunk
-      //     [id: string]: {
-      //       renderedExports: string[]; // exported variable names that were included
-      //       removedExports: string[];  // exported variable names that were removed
-      //       renderedLength: number;    // the length of the remaining code in this module
-      //       originalLength: number;    // the original length of the code in this module
-      //     };
-      //   },
-      //   name: string                   // the name of this chunk as used in naming patterns
-      //   type: 'chunk',                 // signifies that this is a chunk
-      // }
-      console.log('Chunk', chunkOrAsset.modules);
-    }
-  }
-
-  // or write the bundle to disk
+  logger.info("Write to", outputOptions.file);
   await bundle.write(outputOptions);
 }
 
