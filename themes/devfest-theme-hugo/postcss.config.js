5d71bc8860b6214fd04845eddc092059b9544d17
igor <ilaborie@gmail.com>
2020-02-09 12:29:59 +0100

    remove theme

--- themes/devfest-theme-hugo/postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: [
-        require('postcss-preset-env')({
-            autoprefixer: {grid: true},
-            stage: 3
-        }),
-        require('cssnano')({preset: 'default'})
-    ]
-};
